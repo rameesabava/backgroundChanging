eade95ac48ffdab2bd9728db2faf4b93dfa74d19
rameesabava <rameesabava91@gmail.com>
2026-03-11 11:54:54 +0530

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
         <div style={{backgroundColor: backgroundColor, height:'100vh' }} className='text-center d-flex justify-content-center align-items-center'>
            <div style={{borderRadius:'10px'}} className='bg-light p-3 text-center'>
                 <h2>Click the button to show your favourite color!!!!</h2>
-                <div className='d-flex justify-content-evenly align-items-center m-5 w-60'>
+                <div className='d-flex flex-wrap justify-content-evenly align-items-center m-5 w-60'>
                     <Button onClick={() => setBackgroundColor("red")} variant="contained" style={{backgroundColor:'red'}}>RED</Button>
                     <Button onClick={() => setBackgroundColor("blue")} variant="contained">BLUE</Button>
                     <Button onClick={() => setBackgroundColor("green")} variant="contained" style={{backgroundColor:'green'}}>GREEN</Button>
